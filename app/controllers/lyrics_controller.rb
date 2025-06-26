d8fdaa30c65b38d8c1f031c8050a02b9439bdbd0
anikiniki15 <warm.flower@icloud.com>
2025-06-26 20:15:08 +0900

    Lintエラー修正

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -1,5 +1,4 @@
 class LyricsController < ApplicationController
-
   before_action :authenticate_user!
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :lyrics, only: [:new, :create]
+  resources :lyrics, only: [ :new, :create ]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
