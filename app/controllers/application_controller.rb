f69d525f23be5dd34241bb6e03442b55fbb8ebce
anikiniki15 <warm.flower@icloud.com>
2025-06-27 01:50:41 +0900

    ナビゲーションバー追加とログアウトやログインなどボタン追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # These methods will be available in views as well as controllers
+  helper_method :current_user, :user_signed_in?
 end
