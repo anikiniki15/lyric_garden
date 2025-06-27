dca31de8878ceb9679606b8bb03bda1e728c4abf
anikiniki15 <warm.flower@icloud.com>
2025-06-27 18:44:25 +0900

    歌詞一覧ページの実装とkaminariの導入

--- app/models/lyric.rb
+++ app/models/lyric.rb
@@ -4,4 +4,6 @@ class Lyric < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 5000 }
   validates :reference, length: { maximum: 100 }, allow_blank: true
+
+  paginates_per 10  # ← 1ページ10件に変更
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root "pages#home"
-  # root to: "home#index"
+  root "lyrics#index"
 end
