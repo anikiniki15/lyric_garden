e93aceb9293dda1d70f6b04a61af12342366f6e1
anikiniki15 <warm.flower@icloud.com>
2025-06-29 00:01:36 +0900

    バリデーションエラーメッセージ・フラッシュメッセージの日本語化

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,9 @@ module LyricGarden
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # 日本語化
+    config.i18n.default_locale = :ja
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
